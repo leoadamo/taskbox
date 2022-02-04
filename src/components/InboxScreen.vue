b4458ad1243e63365c6c9eadde4b2c62cf6d2a44
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2022-02-04 16:57:13 -0300

    Adding finished code

---
+++ src/components/InboxScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <PureInboxScreen :error="error" />
+</template>
+
+<script>
+import { computed } from 'vue';
+
+import { useStore } from 'vuex';
+
+import PureInboxScreen from './PureInboxScreen';
+
+export default {
+  name: 'InboxScreen',
+
+  components: { PureInboxScreen },
+
+  setup() {
+    const store = useStore();
+
+    const error = computed(() => store.state.error);
+
+    return {
+      error,
+    };
+  },
+};
+</script>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,41 @@
+<template>
+  <PureTaskList
+    :tasks="tasks"
+    @archive-task="archiveTask"
+    @pin-task="pinTask"
+  />
+</template>
+
+<script>
+import PureTaskList from './PureTaskList';
+
+import { computed } from 'vue';
+
+import { useStore } from 'vuex';
+
+export default {
+  components: { PureTaskList },
+
+  name: 'TaskList',
+
+  setup() {
+    const store = useStore();
+
+    const tasks = computed(() => store.state.tasks);
+
+    const archiveTask = (task) => {
+      store.dispatch('archiveTask', task);
+    };
+
+    const pinTask = (task) => {
+      store.dispatch('pinTask', task);
+    };
+
+    return {
+      tasks,
+      archiveTask,
+      pinTask,
+    };
+  },
+};
+</script>
